6073f5e2b3fa2a799aa41899736e612b76a57402
Gabriel <gabdesilets@gmail.com>
2013-03-02 16:34:48 -0500

    sqlfiles

--- Main/MainEspacei/TestMA/change_chat_user.php
+++ Main/MainEspacei/TestMA/change_chat_user.php
@@ -1,15 +1,13 @@
 <?php
-
+session_start();
 $current_onglet = $_POST['onglet'];
-
-if($current_onglet == 'onglet_aidants') {
-	?>
-	<br><ul class="unstyled"><li><p>Marc-Andre Trahan</p></li><li><p>Alexandre Paquin</p></li><li><p>Gabriel Desilets</p></li></ul>
-	<?php
-	//$_RESPONSE['message'] = "Liste des aidants";
+include_once 'user_status.php';
+if($current_onglet == 'onglet_aidants') 
+{
+	echo get_user_status();
 }
 else if($current_onglet == 'onglet_chat') {
-	?>
+?>
 <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
 <script type="text/javascript" src="minichat.js"></script>
 <body onload="refreshChat();">
@@ -26,7 +24,6 @@ Message : <br/><textarea name="message" rows="4" cols="30" id="message_chat" sty
 </p>
 </div>
 </body>
-<script>refreshChat();</script>
 	<?php
 }
 
