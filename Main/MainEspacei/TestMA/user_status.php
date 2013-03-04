eb57156c6f8073bef818088df98bb7ce75b892ac
Marc-André <marc5220@msn.com>
2013-03-03 19:11:37 -0500

    Ajout refresh automatique status des aidants

--- Main/MainEspacei/TestMA/user_status.php
+++ Main/MainEspacei/TestMA/user_status.php
@@ -11,7 +11,24 @@ function get_user_status()
 	//Boucle pour afficher tout les users
 	while($value = mysql_fetch_array($result_status))
 	{
-		echo '<p><a class="nom_aidants" href="#" title="Entrer en communication avec ' . $value['prenom'] . ' ' . $value['nom'] . '"><strong>'.$value['prenom'].'</a>  : </strong>'. $value['status'] .'</p>';
+		echo '<br><p><a class="nom_aidants" href="#" title="Entrer en communication avec ' . $value['prenom'] . ' ' . $value['nom'] . '"><strong>'.$value['prenom'].'</a>  : </strong>'. $value['status'] .'</p>';
 	}
+
+}
+
+function get_status() {
+
+    mysql_connect("localhost", "root", "");
+    mysql_select_db("sitemeut_espace-i2");
+
+    $query_status = "SELECT `status` FROM user_status WHERE uid = " . $_SESSION['uid'];
+    $result_status = mysql_query($query_status);
+
+    while($donnees = mysql_fetch_array($result_status)) {
+
+        echo $donnees['status'];
+
+    }
+
 }
 
