9f1e6a8c6988709a02d11e158b31fb35e9eddba0
bestdramana <bestdramana@github.com>
2013-04-06 13:40:50 -0400

    quelque cleanup

--- Main/MainEspacei/TestMA/calendrier.php
+++ Main/MainEspacei/TestMA/calendrier.php
@@ -27,23 +27,6 @@ function dispatchCalendar()
                  unset($_POST['action']);
                  update($_POST['event']);
              break;
-
-            case "acceptedDispo":
-                unset($_POST['action']);
-                getNotAcceptedDispo($_POST['empId']);
-            break;
-
-            case "acceptEvent":
-                unset($_POST['action']);
-                if(!$_POST['myEventIds'])
-                {
-                    echo 0;
-                }
-                ajax_update_event_state($_POST['myEventIds'],$_POST['isAccepted'],$_POST['eventArrayIds'],$_POST['emp_id']);
-            break;
-            case "show_dispo":
-                show_dispo();
-                break;
         }
     }
 }
@@ -84,38 +67,6 @@ function add($calendriers)
     }
 }
 
-function show_dispo($userId=NULL,$flForAccept=FALSE)
-{
-    $code ="";
-    $data = get_dispo();
-    $num_row = mysql_num_rows($data);
-    $ctr=0;
-
-//Sat Feb 23 2013 16:00:00 GMT-0500 (Est)
-    while ($row = mysql_fetch_assoc($data)) {
-        $code.='{';
-        $code.="id:".$row['id'].',';
-        $code.="title:'".str_replace("'","\'",$row['etu_id'])."',";
-        $code.="detail:'".str_replace("\n","\\n",str_replace("'","\'",$row['etu_id']))."',";
-        $code.="start:'".$row['heuredebut']."',";
-        $code.="end:'".$row['heurefin']."',";
-        $code.="allDay: false,";
-        $code.="backgroundColor: '#4AB840',";
-        $code.="borderColor: '#008238',";
-        $code.="textColor: '#000000'}";
-        $ctr++;
-        if($ctr != $num_row)
-        {
-            $code.=',';
-        }
-
-    }
-    if (!$code)
-    {
-        $code = Array();
-    }
-    echo  $code;
-}
 
 function delete($event_id)
 {
@@ -145,117 +96,4 @@ function update($data)
 
 }
 
-function getNotAcceptedDispo($uid)
-{
-    $data = get_not_accepted_dispo($uid);
 
-    if(mysql_num_rows($data))
-    {
-       echo json_encode(mysql_num_rows($data));
-    }
-
-    else
-    {
-        echo json_encode(0);
-    }
-}
-//myEventIds : myIds, action : 'acceptEvent',isAccepted: flAccepted,eventArrayIds:ids,emp_id:<?php echo $_SESSION['uid']
-function ajax_update_event_state($dataIds,$isAccepted,$arrayIds,$emp_id)
-{
-    $answer = Array();
-
-    foreach($arrayIds as $id)
-    {
-        //check if this emp already accepted this event
-        $event = get_event_state_from_emp($emp_id,$id);
-
-        if(mysql_num_rows($event)==0)
-        {
-
-            $answer['event_id'] = $id;
-            $answer['emp_id'] = $emp_id;
-
-            if($isAccepted==1)
-            {
-                $answer['is_accepted'] = 1;
-                $answer['is_denied'] = 0;
-            }
-            else
-            {
-                $answer['is_accepted'] = 0;
-                $answer['is_denied'] = 1;
-            }
-            insert_emp_answ_events($answer);
-        }
-        else
-        {
-            update_event_emp_state($event,$isAccepted);
-        }
-
-    }
-
-
-}
-
-function sending_reminders()
-{
-    $today = date( "Y-m-d H:i:s" );
-    $trigger_date =  date("Y-m-d H:i:s",strtotime("+1 day"));
-
-
-    $result = get_reminder_events($trigger_date,$today);
-    $data = Array();
-
-    while ($row = mysql_fetch_assoc($result))
-    {
-       array_push($data,$row);
-    }
-
-    return $data;
-
-}
-
-function add_event_reminder($event_id,$user,$start_date,$event_name,$event_desc=NULL)
-{
-
-    $data = Array();
-    $data['reminder_event_id']  = $event_id;
-    $data['reminder_author']    = $user;
-    $data['reminder_date']      = $start_date;
-    $data['reminder_name']      = $event_name;
-    $data['reminder_desc']      = $event_desc;
-
-    insert_event_reminder($data);
-}
-
-function update_reminder($event)
-{
-    $data = Array();
-    $data['reminder_event_id']  = $event['id'];
-    $data['reminder_date']      = $event['start'];
-    $data['reminder_name']      = $event['title'];
-    $data['reminder_desc']      = $event['detail'];
-
-    update_event_reminder($data);
-}
-
-function update_event_emp_state($event,$new_state)
-{
-    $data = Array();
-    while ($row = mysql_fetch_assoc($event))
-    {
-        array_push($data,$row);
-    }
-    if($new_state==1)
-    {
-        $data[0]['is_accepted'] = 1;
-        $data[0]['is_denied'] = 0;
-    }
-    else
-    {
-        $data[0]['is_accepted'] = 0;
-        $data[0]['is_denied'] = 1;
-    }
-
-    update_event_from_emp($data[0]);
-}
--- Main/MainEspacei/TestMA/calendrier_model.php
+++ Main/MainEspacei/TestMA/calendrier_model.php
@@ -27,43 +27,6 @@ function insert_dispo($data)
 
 }
 
-function insert_event_reminder($data)
-{
-    $query =  $query = "insert into reminder_events (reminder_name,reminder_desc,reminder_date,reminder_author,reminder_event_id)
-    VALUES('".mysql_real_escape_string($data['reminder_name'])."','".mysql_real_escape_string($data['reminder_desc'])."',".
-        "STR_TO_DATE('".$data['reminder_date']."','%Y %m %d %H:%i')".",".$data['reminder_author'].",".$data['reminder_event_id'].")";
-
-    $result =  mysql_query($query);
-    if(!$result)
-    {
-        echo"Query failed: " . mysql_error() . " Actual query: " . $query;
-        die();
-    }
-    else
-    {
-        return $result;
-    }
-}
-
-function get_dispo($where = null)
-{
-    $query = "select * from horaire_etu ";
-    if(isset($where))
-    {
-        $query .= "where ".$where;
-    }
-
-    $result =  mysql_query($query);
-    if(!$result)
-    {
-        echo"Query failed: " . mysql_error() . " Actual query: " . $query;
-        die();
-    }
-    else
-    {
-        return $result;
-    }
-}
 
 function delete_dispo($event_id)
 {
@@ -71,7 +34,7 @@ function delete_dispo($event_id)
     $result =  mysql_query($query);
     if(!$result)
     {
-        echo"Query failed: " . mysql_error() . " Actual query: " . $query;
+        echo"Query failed: " . mysql_error();
         die();
     }
     else
@@ -80,20 +43,6 @@ function delete_dispo($event_id)
     }
 }
 
-function delete_reminder($event_id)
-{
-    $query = "DELETE FROM reminder_events WHERE reminder_event_id=".$event_id;
-    $result =  mysql_query($query);
-    if(!$result)
-    {
-        echo"Query failed: " . mysql_error() . " Actual query: " . $query;
-        die();
-    }
-    else
-    {
-        return $result;
-    }
-}
 
 function update_dispo($data)
 {
@@ -102,7 +51,7 @@ function update_dispo($data)
     $result =  mysql_query($query);
     if(!$result)
     {
-        echo"Query failed: " . mysql_error() . " Actual query: " . $query;
+        echo"Query failed: " . mysql_error();
         die();
     }
     else
@@ -119,7 +68,7 @@ function Accept_conv_insert($aidant, $aide)
     $result =  mysql_query($query);
     if(!$result)
     {
-        echo"Query failed: " . mysql_error() . " Actual query: " . $query;
+        echo"Query failed: " . mysql_error();
         die();
     }
     else
